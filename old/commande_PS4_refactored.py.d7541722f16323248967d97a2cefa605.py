39c12e576f8eec6e908cc5185dabd8c7acca9b4c
lefuturiste <spamfree@matthieubessat.fr>
2019-04-01 17:28:10 +0200

    update

--- old/commande_PS4_refactored.py.d7541722f16323248967d97a2cefa605.py
+++ old/commande_PS4_refactored.py.d7541722f16323248967d97a2cefa605.py
@@ -4,42 +4,41 @@
 #version du proramme pour la manette PS4 filaire ou non filaire
 
 #importation du fichier des initialisations
-# importer tout (libraries, initialisations, fonctions)
-from initialisations_PS4 import*
+from initialisations_PS4 import* #importer tout (libraries, initialisations, fonctions)
 
 #CONSTANTES et VARIABLES
 #canaux de la manette PS4
 #button
-BCR = 0  # button bouton croix
-BCE = 1  # button bouton cercle
-BTR = 2  # button bouton trianble
-BCA = 3  # button bouton carré
-BL1 = 4  # button bouton L1 gauche
-BR1 = 5  # button bouton R1 droit
-BL2 = 6  # button bouton L2 gauche - avec axis
-BR2 = 7  # button bouton R2 droit - avec axis
-BSH = 8  # button bouton SHARE
-BOP = 9  # button bouton OPTIONS
-BPS = 10  # button bouton PS4
-BSG = 11  # button bouton stick gauche
-BSD = 12  # button bouton stick droit
-BPT = 13  # button bouton pave tactile
+BCR = 0     #button bouton croix
+BCE = 1     #button bouton cercle
+BTR = 2     #button bouton trianble
+BCA = 3     #button bouton carré
+BL1 = 4     #button bouton L1 gauche
+BR1 = 5     #button bouton R1 droit
+BL2 = 6     #button bouton L2 gauche - avec axis
+BR2 = 7     #button bouton R2 droit - avec axis
+BSH = 8     #button bouton SHARE
+BOP = 9     #button bouton OPTIONS
+BPS = 10    #button bouton PS4
+BSG = 11    #button bouton stick gauche
+BSD = 12    #button bouton stick droit
+BPT = 13    #button bouton pave tactile
 
 #axis
-AGX = 0  # axis stick gauche axe des x
-AGY = 1  # axis stick gauche axe des y
-AL2 = 2  # axis du bouton L2 g - acceleration
-ADX = 3  # axis stick droit axe des x
-ADY = 4  # axis stick droit axe des x
-AR2 = 5  # axis du bouton R2 droit - acceleration
+AGX = 0     #axis stick gauche axe des x
+AGY = 1     #axis stick gauche axe des y
+AL2 = 2     #axis du bouton L2 g - acceleration
+ADX = 3     #axis stick droit axe des x
+ADY = 4     #axis stick droit axe des x
+AR2 = 5     #axis du bouton R2 droit - acceleration
 
 #hat
-HAT = 0  # hat croix directionnelle
+HAT = 0     #hat croix directionnelle
 
-werobot = 1  # break quand continuer égal 0
-VT = 25  # vitesse minimum
-vt = VT  # initialisation de la variable vt à la vitaesse minimum
-S = 0  # seuil stick - si zéro pas de seuil
+werobot = 1 #break quand continuer égal 0
+VT = 25     #vitesse minimum
+vt = VT     #initialisation de la variable vt à la vitaesse minimum
+S = 0       #seuil stick - si zéro pas de seuil
 
 oldtimePince = 0
 oldtimePdroit = 0
@@ -51,11 +50,8 @@ isHatLeftRepressed = True
 isHatRightRepressed = True
 
 #FONCTIONS
-
-
-def axesZero(seuil):
-    return abs(adx) <= seuil and abs(ady) <= seuil and abs(agx) <= seuil and abs(agy) <= seuil and (abs(al2+1) <= seuil or abs(al2) <= seuil) and (abs(ar2+1) <= seuil or abs(ar2) <= seuil)
-
+def axesZero(seuil) :
+    return abs(adx)<=seuil and abs(ady)<=seuil and abs(agx)<=seuil and abs(agy)<=seuil and (abs(al2+1)<=seuil or abs(al2)<=seuil) and (abs(ar2+1)<=seuil or abs(ar2)<=seuil)
 
 #PROGRAMME
 ar()
@@ -70,51 +66,52 @@ pygame.init()
 while werobot:
     for event in pygame.event.get():
         #print("event = ",event)
-        ##        #if event.type == pygame.JOYBUTTONDOWN:
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BPS: #bouton PS4
-        ##            werobot = 0 #sortie du programme
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BTR: #bouton TRIANGLE
-        ##            baseGoto(baseHaut,v = 70)
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BCE: #bouton CERCLE
-        ####				if monRobot.m3.present_position < 10 : # La pince est au niveau du grd distributeur
-        ####					baseGoto(baseGdistriM,v = 70)
-        ####				else : # La pince est au niveau du sol
-        ####					baseGoto(baseMilieu, v = 70)
-        ####				time.sleep(1)
-        ##            baseGoto(baseHautDroit, v = 70)
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BCA: #bouton CARRE
-        ####				if monRobot.m3.present_position < 10 : # La pince est au niveau du grd distributeur
-        ####					baseGoto(baseGdistriM,v = 70)
-        ####				else : # La pince est au niveau du sol
-        ####					baseGoto(baseMilieu, v = 70)
-        ####				time.sleep(1)
-        ##            baseGoto(baseHautGauche, v = 70)
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BCR: #bouton CROIX
-        ##            baseGoto(baseBas, v = 70)
-        ####            elif event.button == BL1: #bouton L1
-        ####                baseGoto(baseGdistri, v = 70)
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BR1: #bouton R1
-        ##            if isClawOpen:
-        ##                pinceGoto(pinceFermee, v = 70)
-        ##            else :
-        ##                pinceGoto(pinceOuverte, v = 70)
-        ##            isClawOpen = not isClawOpen
-        ####            elif event.button == BSG: #bouton Stick Gauche
-        ####                baseGoto(baseGold, v = 70)
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BSH: #bouton SHARE
-        ##            pistonGauche() # Toggle le piston gauche
-        ##        if event.type == pygame.JOYBUTTONDOWN and event.button == BOP: #bouton OPTION
-        ##            pistonDroit() # Toggle le piston droit
-        ##
-        ##        hat = mon_joystick.get_hat(HAT)
-        ####        if hat[0] == -1 :
-        ####            rah(vt)
-        ####        if hat[0] == 1 :
-        ####            rh(vt)
-        ##        if hat[1] == 1 : #Serre un peu plus la pince
-        ##            bougePince(1)
-        ##        if hat[1] == -1 : #Desserre un peu la pince
-        ##            bougePince(-1)
+##        #if event.type == pygame.JOYBUTTONDOWN:
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BPS: #bouton PS4
+##            werobot = 0 #sortie du programme
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BTR: #bouton TRIANGLE
+##            baseGoto(baseHaut,v = 70)
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BCE: #bouton CERCLE
+####				if monRobot.m3.present_position < 10 : # La pince est au niveau du grd distributeur
+####					baseGoto(baseGdistriM,v = 70)
+####				else : # La pince est au niveau du sol
+####					baseGoto(baseMilieu, v = 70)
+####				time.sleep(1)
+##            baseGoto(baseHautDroit, v = 70)
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BCA: #bouton CARRE
+####				if monRobot.m3.present_position < 10 : # La pince est au niveau du grd distributeur
+####					baseGoto(baseGdistriM,v = 70)
+####				else : # La pince est au niveau du sol
+####					baseGoto(baseMilieu, v = 70)
+####				time.sleep(1)
+##            baseGoto(baseHautGauche, v = 70)
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BCR: #bouton CROIX
+##            baseGoto(baseBas, v = 70)
+####            elif event.button == BL1: #bouton L1
+####                baseGoto(baseGdistri, v = 70)
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BR1: #bouton R1
+##            if isClawOpen:
+##                pinceGoto(pinceFermee, v = 70)
+##            else :
+##                pinceGoto(pinceOuverte, v = 70)
+##            isClawOpen = not isClawOpen
+####            elif event.button == BSG: #bouton Stick Gauche
+####                baseGoto(baseGold, v = 70)
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BSH: #bouton SHARE
+##            pistonGauche() # Toggle le piston gauche
+##        if event.type == pygame.JOYBUTTONDOWN and event.button == BOP: #bouton OPTION
+##            pistonDroit() # Toggle le piston droit
+##
+##        hat = mon_joystick.get_hat(HAT)
+####        if hat[0] == -1 :
+####            rah(vt)
+####        if hat[0] == 1 :
+####            rh(vt)
+##        if hat[1] == 1 : #Serre un peu plus la pince
+##            bougePince(1)
+##        if hat[1] == -1 : #Desserre un peu la pince
+##            bougePince(-1)
+			
 
         mon_joystick = pygame.joystick.Joystick(0)
         mon_joystick.init()
@@ -143,35 +140,35 @@ while werobot:
         #         if i == BL1:
         #             pinceGoto(pinceOuverte, v=70)
         for i in range(mon_joystick.get_numbuttons()):
-            if mon_joystick.get_button(i) == 1:
+            if mon_joystick.get_button(i) == 1 :
                 #print("bouton {} pressed !".format(i))
                 if i == BPS:
                     werobot = 0
-                if i == BTR:
-                    baseGoto(baseHaut, v=70)
-                if i == BCE:
-                    baseGoto(baseHautDroit, v=70)
-                if i == BCA:
-                    baseGoto(baseHautGauche, v=70)
-                if i == BCR:
-                    baseGoto(baseGdistri, v=70)
-                if i == BSH:
-                    pinceGoto([40, 40], v=70)
-                if i == BOP:
-                    pinceGoto([40, 40], v=70)
+                if i == BTR :
+                    baseGoto(baseHaut,v = 70)
+                if i == BCE :
+                    baseGoto(baseHautDroit, v = 70)
+                if i == BCA :
+                    baseGoto(baseHautGauche, v = 70)
+                if i == BCR :
+                    baseGoto(baseGdistri, v = 70)
+                if i == BSH :
+                    pinceGoto([40,40],v=70)
+                if i == BOP :
+                    pinceGoto([40,40],v=70)
                 """ if i == BR1:
                     pinceGoto(pinceFermee, v= 70)
                     ser.write(b"LED\n") """
                 if i == BL1:
-                    if isBL1RePressed:
-                        if isClawOpen:
-                            pinceGoto(pinceFermee, v=70)
-                        else:
+                    if isBL1RePressed :
+                        if isClawOpen :
+                            pinceGoto(pinceFermee, v = 70)
+                        else :
                             pinceGoto(pinceOuverte, v=70)
                         isClawOpen = not isClawOpen
                         ser.write(b"LED\n")
                         isBL1RePressed = False
-            else:
+            else :
                 if i == BL1:
                     isBL1RePressed = True
 ##                if i == BR1:
@@ -188,15 +185,15 @@ while werobot:
 ##                    pistonDroit()
         for i in range(mon_joystick.get_numhats()):
             hat = mon_joystick.get_hat(i)
-            if hat[1] == 1 and isHatUpRepressed:
+            if hat[1] == 1 and isHatUpRepressed :
                 ser.write(b"plus\n")
                 isHatUpRepressed = False
                 #bougePince(2)
-            if hat[1] == 0:
+            if hat[1] == 0 :
                 isHatUpRepressed = True
                 isHatDownRepressed = True
-
-            if hat[1] == -1 and isHatDownRepressed:
+            
+            if hat[1] == -1 and isHatDownRepressed :
                 ser.write(b"moins\n")
                 isHatDownRepressed = False
                 #bougePince(-2)
@@ -206,7 +203,7 @@ while werobot:
 ##                if (time.clock() - oldtimePdroit) > 0.3 :
 ##                    pistonDroit()
 ##                    oldtimePdroit = time.clock()
-            if hat[0] == 0:
+            if hat[0] == 0 :
                 isHatRightRepressed = True
                 isHatLeftRepressed = True
             if hat[0] == -1 and isHatLeftRepressed:
@@ -215,22 +212,22 @@ while werobot:
 ##                if (time.clock() - oldtimePgauche) > 0.3 :
 ##                    pistonGauche()
 ##                    oldtimePgauche = time.clock()
-        #time.sleep(0.1)
+        #time.sleep(0.1)           
         pygame.event.clear()
         agx = mon_joystick.get_axis(AGX)
         agy = -mon_joystick.get_axis(AGY)
         adx = mon_joystick.get_axis(ADX)
         ady = -mon_joystick.get_axis(ADY)
         hat = mon_joystick.get_hat(HAT)
-        al2 = mon_joystick.get_axis(AL2)  # accélération
-        ar2 = mon_joystick.get_axis(AR2)  # accélération
+        al2 = mon_joystick.get_axis(AL2) #accélération
+        ar2 = mon_joystick.get_axis(AR2) #accélération
         if axesZero(S):
             ar()
         else:
             if al2 == -1:
                 vt = VT
-            elif al2 != 0:
-                vt = mappyt(al2, -1, 1, 0, 1) * 100
+            elif al2 != 0 :
+                vt = mappyt(al2,-1,1,0,1) * 100
                 #print("al2 = {0} et vt = {1}".format(al2,vt))
             if ady < 0.5 * adx and ady >= -0.5 * adx:
                 te(vt)
@@ -239,7 +236,7 @@ while werobot:
             if ady > 2 * adx and ady >= -2 * adx:
                 #print("ordre recu !")
                 tn(vt)
-            if ady < -2 * adx and ady >= -0.5 * adx:
+            if ady < -2* adx and ady >= -0.5 * adx:
                 tno(vt)
             if ady < -0.5 * adx and ady >= 0.5 * adx:
                 to(vt)
@@ -249,17 +246,17 @@ while werobot:
                 ts(vt)
             if ady > -2 * adx and ady <= -0.5 * adx:
                 tse(vt)
-            if agy > 2 * agx and agy >= -2 * agx:  # Lève le bras, stick gauche en haut
+            if agy > 2 * agx and agy >= -2 * agx: #Lève le bras, stick gauche en haut
                 lb(30)
-            if agy < 2 * agx and agy <= -2 * agx:  # Baisse le bras, stick gauche en bas
+            if agy < 2 * agx and agy <= -2 * agx: #Baisse le bras, stick gauche en bas
                 bb(20)
             if agy < 0.5 * agx and agy >= -0.5 * agx:
                 rh(vt)
             if agy < -0.5 * agx and agy >= 0.5 * agx:
                 rah(vt)
-
+            
                 #~ if abs(ady) <= S and abs(adx) <= S and abs(hat[0]) <= 0 :
-                #~ ar()
+                    #~ ar()
             #~ if hat[0] == -1:
                 #~ rah(vt)
             #~ if hat[0] == 1 :
@@ -269,5 +266,5 @@ esc_zero()
 monRobot.close()
 ##baseBras.join()
 ##posBase.put(0,block=False) #ordre non executé permetant l'arrêt du thread.
-print("")
-print("fin du programme")
+print ("")
+print ("fin du programme")
--- old/initialisations_PS4.py.e88172a3d1523904ffbba2e3857c4a38.py
+++ old/initialisations_PS4.py.e88172a3d1523904ffbba2e3857c4a38.py
@@ -7,15 +7,8 @@
 
 
 #version du proramme pour la manette PS4 filaire ou non filaire
-import sys  # , dcEncodeur, queue
-import pygame
-import Adafruit_PCA9685
-import serial
-import pypot.robot
-import time
-import pypot.dynamixel
-HZ = 50  # fréquence du signal vers les ESC
-ESC_BRAS = 2
+HZ = 50     #fréquence du signal vers les ESC
+ESC_BRAS = 2 
 SERVO_PISTON_G = 3
 SERVO_PISTON_D = 4
 # variable contenant la position en degré des pistons
@@ -27,12 +20,13 @@ pg_pos = 100
 pg_init = 100
 pg_bloc = 325
 pg_pouss = 365
-# liste des ESC (avant gauche, avant droit, arrière gauche, arrière droit)
-LESC = (6, 5, 0, 1)
+LESC = (6,5,0,1) #liste des ESC (avant gauche, avant droit, arrière gauche, arrière droit)
+
+import Adafruit_PCA9685, time, pygame, sys #, dcEncodeur, queue
 
 
 #initialiser le PCA9685 Ã  l'adresse par défaut 0x40
-servo = Adafruit_PCA9685.PCA9685()
+servo=Adafruit_PCA9685.PCA9685()
 
 #définir la fréquence du signal de sortie à 50hz
 servo.set_pwm_freq(HZ)
@@ -43,163 +37,126 @@ pygame.init()
 pygame.joystick.init()
 
 #procédure d'initialisation d'un ESC
-
-
 def ini_esc(slot):
-    # 307 est le signal neutre sous 50 Hz (1.5 / 20 x 4096 = 307)
-    servo.set_pwm(slot, 0, 307)
+    servo.set_pwm(slot,0,307) #307 est le signal neutre sous 50 Hz (1.5 / 20 x 4096 = 307)
     time.sleep(1)
 
-
 def initialiser_esc():
-    ini_esc(LESC[0])  # initialiser l'ESC roue avant gauche slot 0
-    ini_esc(LESC[1])  # initialiser l'ESC roue avant droite slot 1
-    ini_esc(LESC[2])  # initialiser l'ESC roue arrière gauche slot 14
-    ini_esc(LESC[3])  # initialiser l'ESC roue arrière droite slot 15
-    ini_esc(ESC_BRAS)  # initialiser l'ESC base du bras slot ESC_BRAS
-
+    ini_esc(LESC[0]) #initialiser l'ESC roue avant gauche slot 0
+    ini_esc(LESC[1]) #initialiser l'ESC roue avant droite slot 1
+    ini_esc(LESC[2]) #initialiser l'ESC roue arrière gauche slot 14
+    ini_esc(LESC[3]) #initialiser l'ESC roue arrière droite slot 15
+    ini_esc(ESC_BRAS) #initialiser l'ESC base du bras slot ESC_BRAS
 
 def init_servos():
     global pd_init, pg_init
-    servo.set_pwm(SERVO_PISTON_D, 0, pd_init)
-    servo.set_pwm(SERVO_PISTON_G, 0, pg_init)
+    servo.set_pwm(SERVO_PISTON_D,0,pd_init)
+    servo.set_pwm(SERVO_PISTON_G,0,pg_init)
 
 #équivalent de la fonction map() de arduino
-
-
 def mappyt(x, in_min, in_max, out_min, out_max):
     return (x-in_min) * (out_max-out_min) / (in_max-in_min) + out_min
 
 #fonction esc pour une vitesse de moteur de -100 à 100()
-
-
 def esc(v):
-    return round(mappyt(v, 0, 100, 307, 410))
+    return round(mappyt(v,0,100,307,410))
 
 #translation nord (avance)
-
-
 def tn(vit):
     a = esc(vit)
-    servo.set_pwm(LESC[0], 0, a)  # canal 0 : roue avant gauche
-    servo.set_pwm(LESC[1], 0, a)  # canal 1 : roue avant droite
-    servo.set_pwm(LESC[2], 0, a)  # canal 14 : roue arrière gauche
-    servo.set_pwm(LESC[3], 0, a)  # canal 15 : roue arrière droite
+    servo.set_pwm(LESC[0],0,a) #canal 0 : roue avant gauche
+    servo.set_pwm(LESC[1],0,a) #canal 1 : roue avant droite
+    servo.set_pwm(LESC[2],0,a) #canal 14 : roue arrière gauche
+    servo.set_pwm(LESC[3],0,a) #canal 15 : roue arrière droite
 
 #translation sud (recule)
-
-
 def ts(vit):
     tn(-vit)
 
 #translation est (droite)
-
-
 def te(vit):
     a = esc(vit)
     r = esc(-vit)
-    servo.set_pwm(LESC[0], 0, a)  # roue avant gauche
-    servo.set_pwm(LESC[1], 0, r)  # roue avant droite
-    servo.set_pwm(LESC[2], 0, r)  # roue arrière gauche
-    servo.set_pwm(LESC[3], 0, a)  # roue arrière droit
+    servo.set_pwm(LESC[0],0,a) #roue avant gauche
+    servo.set_pwm(LESC[1],0,r) #roue avant droite
+    servo.set_pwm(LESC[2],0,r) #roue arrière gauche
+    servo.set_pwm(LESC[3],0,a) #roue arrière droit
 
 #translation ouest (gauche)
-
-
 def to(vit):
     te(-vit)
 
 #rotation horaire
-
-
 def rh(vit):
     a = esc(vit)
     r = esc(-vit)
-    servo.set_pwm(LESC[0], 0, a)  # roue avant gauche
-    servo.set_pwm(LESC[1], 0, r)  # roue avant droite
-    servo.set_pwm(LESC[2], 0, a)  # roue arrière gauche
-    servo.set_pwm(LESC[3], 0, r)  # roue arrière droite
+    servo.set_pwm(LESC[0],0,a) #roue avant gauche
+    servo.set_pwm(LESC[1],0,r) #roue avant droite
+    servo.set_pwm(LESC[2],0,a) #roue arrière gauche
+    servo.set_pwm(LESC[3],0,r) #roue arrière droite
 
 # rotation anti horaire
-
-
 def rah(vit):
     rh(-vit)
 
 # translation nord est
-
-
 def tne(vit):
     a = esc(vit)
     s = esc(0)
-    servo.set_pwm(LESC[0], 0, a)  # roue avant gauche
-    servo.set_pwm(LESC[1], 0, s)  # roue avant droite
-    servo.set_pwm(LESC[2], 0, s)  # roue arrière gauche
-    servo.set_pwm(LESC[3], 0, a)  # roue arrière droite
+    servo.set_pwm(LESC[0],0,a) #roue avant gauche
+    servo.set_pwm(LESC[1],0,s) #roue avant droite
+    servo.set_pwm(LESC[2],0,s) #roue arrière gauche
+    servo.set_pwm(LESC[3],0,a) #roue arrière droite
 
 #translation sud ouest
-
-
 def tso(vit):
-    tne(-vit)
+    tne (-vit)
 
 #translation nord ouest
-
-
 def tno(vit):
     a = esc(vit)
     s = esc(0)
-    servo.set_pwm(LESC[0], 0, s)  # roue avant gauche
-    servo.set_pwm(LESC[1], 0, a)  # roue avant droit
-    servo.set_pwm(LESC[2], 0, a)  # roue arrière gauche
-    servo.set_pwm(LESC[3], 0, s)  # roue arrière droit
+    servo.set_pwm(LESC[0],0,s) #roue avant gauche
+    servo.set_pwm(LESC[1],0,a) #roue avant droit
+    servo.set_pwm(LESC[2],0,a) #roue arrière gauche
+    servo.set_pwm(LESC[3],0,s) #roue arrière droit
 
 #translation sud est
-
-
 def tse(vit):
     tno(-vit)
 
 #leve le bras:
-
-
 def lb(vit):
     a = esc(-vit)
-    servo.set_pwm(ESC_BRAS, 0, a)
+    servo.set_pwm(ESC_BRAS,0,a)
 
 #baisse bras:
-
-
 def bb(vit):
     lb(-vit)
 
 #arrêt du robot
-
-
 def ar():
     a = esc(0)
-    servo.set_pwm(LESC[0], 0, a)
-    servo.set_pwm(LESC[1], 0, a)
-    servo.set_pwm(LESC[2], 0, a)
-    servo.set_pwm(LESC[3], 0, a)
-    servo.set_pwm(ESC_BRAS, 0, a)  # arrêt base bras
-    #servo.set_pwm(SERVO_PISTON_G,0,a)
-    #servo.set_pwm(SERVO_PISTON_D,0,a)
+    servo.set_pwm(LESC[0],0,a)
+    servo.set_pwm(LESC[1],0,a)
+    servo.set_pwm(LESC[2],0,a)
+    servo.set_pwm(LESC[3],0,a)
+    servo.set_pwm(ESC_BRAS,0,a) #arrêt base bras
+    #servo.set_pwm(SERVO_PISTON_G,0,a) 
+    #servo.set_pwm(SERVO_PISTON_D,0,a) 
 
 #zéro signal vers les ESC
-
-
 def esc_zero():
-    a = 4096
-    servo.set_pwm(LESC[0], 0, a)
-    servo.set_pwm(LESC[1], 0, a)
-    servo.set_pwm(LESC[2], 0, a)
-    servo.set_pwm(LESC[3], 0, a)
-    servo.set_pwm(ESC_BRAS, 0, a)
+    a=4096
+    servo.set_pwm(LESC[0],0,a)
+    servo.set_pwm(LESC[1],0,a)
+    servo.set_pwm(LESC[2],0,a)
+    servo.set_pwm(LESC[3],0,a)
+    servo.set_pwm(ESC_BRAS,0,a)
 
+def vitesse (vit):
+    return round(mappyt(vit,0,1,307,410))
 
-def vitesse(vit):
-    return round(mappyt(vit, 0, 1, 307, 410))
 
 
 def pistonGauche():
@@ -208,8 +165,7 @@ def pistonGauche():
         pg_pos = pg_pouss
     else:
         pg_pos = pg_init
-    servo.set_pwm(SERVO_PISTON_G, 0, pg_pos)
-
+    servo.set_pwm(SERVO_PISTON_G,0,pg_pos)
 
 def pistonDroit():
     global pd_pos, pd_init, pd_pouss, SERVO_PISTON_D
@@ -217,7 +173,7 @@ def pistonDroit():
         pd_pos = pd_pouss
     else:
         pd_pos = pd_init
-    servo.set_pwm(SERVO_PISTON_D, 0, pd_pos)
+    servo.set_pwm(SERVO_PISTON_D,0,pd_pos)
 
 
 #initialiser tous les ESC et les servos
@@ -227,6 +183,7 @@ print("")
 init_servos()
 print("Les servos sont initialisés")
 print("")
+
 
 
 #On compte les joysticks
@@ -254,6 +211,10 @@ else:
 #####################################################################
 
 
+import pypot.dynamixel
+import time
+import pypot.robot
+
 # Procédure intermédiaire pour récupérer le contrôle du bras en cas de fail
 
 
@@ -264,7 +225,7 @@ IDs = moteurs.scan(range(10))
 
 for a in IDs:
     print(moteurs.get_present_position((a,)))
-    moteurs.set_moving_speed({a: 30})
+    moteurs.set_moving_speed({a:30})
 
 # position initiale : [-100,0,100,100,100]
 ##moteurs.set_goal_position({2: 100.0})
@@ -291,7 +252,7 @@ robot_config = {
     },
     'motors': {
         'm2': {
-            'orientation': 'direct',
+           'orientation': 'direct',
             'type': 'AX-12',
             'id': 6,
             'angle_limit': [-150.0, 150.0],
@@ -303,7 +264,8 @@ robot_config = {
             'id': 3,
             'angle_limit': [-150.0, 150.0],
             'offset': 0.0
-        },
+        }
+            ,
         'm4': {
             'orientation': 'direct',
             'type': 'AX-12',
@@ -333,74 +295,63 @@ monRobot = pypot.robot.from_config(robot_config)
 ##baseBras.start()
 
 #Fonction de déplacement de la base du bras
-
-
-def baseGoto(L, v=30, wait=False):
+def baseGoto(L,v=30,wait = False):
     n = len(monRobot.base)
     if len(L) == n:
         #posBase.put(L[0],block=False)
-        if not wait:
-            for i in range(n):
+        if not wait :            
+            for i in range(n) :
                 monRobot.base[i].moving_speed = v
                 monRobot.base[i].goal_position = L[i]
                 #print("moteur {} : speed = {} et goal pos = {}".format(i,v,L[i]))
         else:
-            for i in range(1, n):
+            for i in range(1,n):
                 delta = abs(monRobot.base[i].present_position - L[i])
                 duration = delta / v
-                monRobot.base[i].goto_position(L[i], duration, wait=True)
+                monRobot.base[i].goto_position(L[i],duration,wait=True)
     else:
-        print(
-            "ERREUR : il y a {} moteurs et la liste contient {} valeurs".format(n, len(L)))
+        print("ERREUR : il y a {} moteurs et la liste contient {} valeurs".format(n,len(L)))
 
 #Fonction de déplacement de la pince
-
-
-def pinceGoto(L, v=30, wait=False):
+def pinceGoto(L,v=30,wait = False):
     n = len(monRobot.pince)
     if len(L) == n:
-        if not wait:
-            for i in range(n):
+        if not wait :
+            for i in range(n) :
                 monRobot.pince[i].moving_speed = v
                 monRobot.pince[i].goal_position = L[i]
         else:
             for i in range(n):
                 delta = abs(monRobot.pince[i].present_position - L[i])
                 duration = delta / v
-                monRobot.pince[i].goto_position(L[i], duration, wait=True)
+                monRobot.pince[i].goto_position(L[i],duration,wait=True)
     else:
-        print(
-            "ERREUR : il y a {} moteurs et la liste contient {} valeurs".format(n, len(L)))
-
+        print("ERREUR : il y a {} moteurs et la liste contient {} valeurs".format(n,len(L)))
 
 def bougePince(val):
-    for m in monRobot.pince:
+    for m in monRobot.pince :
         m.moving_speed = 30
         m.goal_position = m.present_position + val
 
+baseMilieu = [0,102]  # position de repos basse
+baseHaut = [0,0] # position verticale - inutile ?
+baseBas = [-1,10] # position basse
+baseGdistri = [2,-29] # position pour attraper les palets dans le grd distributeur
+baseGdistriM = [-50,2,52] # position intermédiaire après saisie sur le grd distributeur
+baseHautDroit = [-60,-27] # position pour dépôt goulotte droite A COMPLETER
+baseHautGauche = [65,-22] # position pour dépôt goulotte gauche A COMPLETER
+baseGold = [0,0,0] # position pour pousser le Goldenium et pour l'accelérateur de particule A COMPLETER
 
-baseMilieu = [0, 102]  # position de repos basse
-baseHaut = [0, 0]  # position verticale - inutile ?
-baseBas = [-1, 10]  # position basse
-# position pour attraper les palets dans le grd distributeur
-baseGdistri = [2, -29]
-# position intermédiaire après saisie sur le grd distributeur
-baseGdistriM = [-50, 2, 52]
-baseHautDroit = [-60, -27]  # position pour dépôt goulotte droite A COMPLETER
-baseHautGauche = [65, -22]  # position pour dépôt goulotte gauche A COMPLETER
-# position pour pousser le Goldenium et pour l'accelérateur de particule A COMPLETER
-baseGold = [0, 0, 0]
-
-pinceOuverte = [89, 89]
-pinceFermee = [97, 97]
+pinceOuverte = [89,89]
+pinceFermee = [97,97]
 
 monRobot.power_up()
 
 baseGoto(baseMilieu)
-pinceGoto(pinceOuverte, v=70)
+pinceGoto(pinceOuverte, v = 70)
 isClawOpen = True
 
 ########################### SERIAL ####################################
+import serial
 
-ser = serial.Serial(port="/dev/ttyACM0", baudrate=9600, parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+ser = serial.Serial( port="/dev/ttyACM0", baudrate = 9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
